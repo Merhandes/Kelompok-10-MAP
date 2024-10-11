97a9ebb6cf0434314396f9dcf296c1f91af9748b
Merhandes Gunawan <merhandes.gunawan@student.umn.ac.id>
2024-10-11 11:05:28 +0700

    Added Icon & Splash Screen

--- app/src/main/java/com/example/valetparking/HomeActivity.kt
+++ app/src/main/java/com/example/valetparking/HomeActivity.kt
@@ -1,24 +1,16 @@
 package com.example.valetparking
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.util.logging.Handler
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        supportActionBar?.hide()
-        android.os.Handler().postDelayed({
-            val intent = Intent(this@MainActivity, HomeActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, 3000)
+        setContentView(R.layout.activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
