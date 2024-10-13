0f69b3f136326c11dd2486285855300c3d74a6f5
Merhandes Gunawan <merhandes.gunawan@student.umn.ac.id>
2024-10-13 14:05:07 +0700

    Added Register Activity

--- app/src/main/java/com/example/valetparking/HomeActivity.kt
+++ app/src/main/java/com/example/valetparking/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.valetparking
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,12 @@ import androidx.core.view.WindowInsetsCompat
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val btnClick: Button = findViewById(R.id.btn)
+
+        btnClick.setOnClickListener{
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
         }
     }
 }
