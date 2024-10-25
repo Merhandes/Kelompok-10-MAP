e396d95f5ffa2677a145b7255e631649a29293aa
Marcellus246 <125130831+Marcellus246@users.noreply.github.com>
2024-10-25 20:43:08 +0700

    autentikasi

--- app/src/main/java/com/example/valetparking/LoginActivity.kt
+++ app/src/main/java/com/example/valetparking/LoginActivity.kt
@@ -3,20 +3,44 @@ package com.example.valetparking
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
         val btnLogin: Button = findViewById(R.id.btnLogin)
         val btnSignup: TextView = findViewById(R.id.btnSignup)
         val btnForgot: TextView = findViewById(R.id.btnForgot)
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
 
-        btnLogin.setOnClickListener{
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+        btnLogin.setOnClickListener {
+            val email = etEmail.text.toString()
+            val password = etPassword.text.toString()
+
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Login sukses, pindah ke halaman utama
+                        val intent = Intent(this, HomeActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    } else {
+                        // Gagal login
+                        Toast.makeText(this, "Login gagal: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
 
         btnSignup.setOnClickListener{
